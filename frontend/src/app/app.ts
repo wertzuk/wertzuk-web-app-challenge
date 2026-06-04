6c2e44f288a2172c24855031405c5aa9c0a5b94f
Pascal Barzen <pascalb95@gmx.de>
2026-06-04 14:32:26 +0200

    remove RouterOutlet

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [Home],
   templateUrl: './app.html',
 })
 export class App {
