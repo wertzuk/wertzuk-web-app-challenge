f26918cab9f01dc455f4990ccd9fd57455552cff
Pascal Barzen <pascalb95@gmx.de>
2026-06-04 11:04:57 +0200

    update displayed amounts and improve previous amount handling

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Denomination } from '../models';
 
@@ -10,30 +10,35 @@ import { Denomination } from '../models';
 export class Home {
   amount: number | null = null;
 
-  previousAmount = signal<number | null>(null);
+
+  previousAmount: number | null = null;
   isLoading = signal(false);
   isCompleted = signal(false);
   isUsingFrontend = signal(false);
   isFirstCalculation = signal(true);
 
+  displayedCurrentAmount: number | null = null;
+  displayedPreviousAmount: number | null = null;
+
   denominations = signal<Denomination[]>([]);
 
   async calculate() {
     if (this.isLoading()) return;
-    if (this.amount === this.previousAmount()) {
+
+    if (this.amount === this.previousAmount) {
       alert('Selber Betrag wie in der vorherigen Berechnung. Bitte ändern Sie den Betrag, um erneut zu berechnen.');
       return;
     }
 
     this.isLoading.set(true);
 
-    if (this.previousAmount()) {
+    if (this.previousAmount) {
       this.isFirstCalculation.set(false);
     }
 
     let url = `http://localhost:8080/calculate?amount=${this.amount}`;
-    if (this.previousAmount()) {
-      url += `&previousAmount=${this.previousAmount()}`;
+    if (this.previousAmount) {
+      url += `&previousAmount=${this.previousAmount}`;
     }
 
     console.log('Call Endpoint', url);
@@ -42,11 +47,12 @@ export class Home {
     this.isLoading.set(false);
     this.isCompleted.set(true);
     if (!response.ok) {
-      alert('Error calculating amount');
+      alert('Betrag konnte nicht berechnet werden.');
       return;
     }
-
-    this.previousAmount.set(this.amount);
+    this.displayedPreviousAmount = this.previousAmount;
+    this.displayedCurrentAmount = this.amount;
+    this.previousAmount = this.amount;
     this.denominations.set((await response.json()) as Denomination[]);
 
     this.amount = null;
