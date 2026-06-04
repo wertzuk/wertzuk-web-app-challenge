39d6cbc4201cc9f3d7ff4d0523bfa4f333dfc0a8
Pascal Barzen <pascalb95@gmx.de>
2026-06-04 09:38:58 +0200

    call backend endpoint on form submit

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Denomination } from '../models';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
 
 })
 export class Home {
+  amount: number | null = null;
+
+  previousAmount = signal<number | null>(null);
+  isLoading = signal(false);
+  isCompleted = signal(false);
+  isUsingFrontend = signal(false);
+  isFirstCalculation = signal(true);
+
+  denominations = signal<Denomination[]>([]);
+
+
+  async calculate() {
+    console.log('Calculate Denomatinations for: ', this.amount);
+    if (this.previousAmount()) {
+      this.isFirstCalculation.set(false);
+    }
+
+    this.isLoading.set(true);
+    let url = `http://localhost:8080/calculate?amount=${this.amount}`;
+    if (this.previousAmount()) {
+      url += `&previousAmount=${this.previousAmount()}`
+    }
+
+    console.log('Call Endpoint', url);
+
+    const response = await fetch(url);
+    this.isLoading.set(false);
+    this.isCompleted.set(true);
+    if (!response.ok) {
+      alert('Error calculating amount');
+      return;
+    }
+
+    this.previousAmount.set(this.amount);
+    this.denominations.set(await response.json() as Denomination[]);
+  }
 
 }
